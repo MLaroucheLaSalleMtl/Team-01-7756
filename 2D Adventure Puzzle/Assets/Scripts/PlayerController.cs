c8f5f5add8a81cf4dbc43a3f7b57c3d0981c9a85
Robert <roobtran@gmail.com>
2019-01-28 18:24:21 -0500

    + Added Character Movement/Animation
    + Added Character movement (left/right)
    + Adeed Character walking animation/Idle.

---
+++ 2D Adventure Puzzle/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float characterSpeed = 5.0f;
+    [SerializeField] float localScale = 5.0f;
+    private Animator myAnimator;
+    public Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Function to move the player
+        MainMovement();
+
+
+    }
+
+    public void MainMovement()
+    {
+        //Check if the player move right
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            //Move right
+            rigidbody.velocity = new Vector2(characterSpeed, rigidbody.velocity.y);
+            //Rotate the char depending of the side
+            transform.localScale = new Vector2(localScale, localScale);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            //Move left
+            rigidbody.velocity = new Vector2(-characterSpeed, rigidbody.velocity.y);
+            //Rotate the char depending of the side
+            transform.localScale = new Vector2(-localScale, localScale);
+        }
+        else
+        // If no movement
+        {
+            rigidbody.velocity = new Vector2(0, rigidbody.velocity.y);
+        }
+        //Value of the animator
+        myAnimator.SetFloat("Speed", Mathf.Abs(rigidbody.velocity.x));
+    }
+
+
+}
