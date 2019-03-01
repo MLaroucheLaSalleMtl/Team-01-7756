8b32b9aaa0ac855471e709e184e71c3d62428718
Jay <mhanhx@gmail.com>
2019-03-01 11:25:07 -0500

    Fixed some bugs
    Bus in testlevel2

--- 2D Adventure Puzzle/Assets/Scripts/CollectStar.cs
+++ 2D Adventure Puzzle/Assets/Scripts/CollectStar.cs
@@ -26,8 +26,7 @@ public class CollectStar : MonoBehaviour
         if(collision.gameObject.tag == "Player")
         {
             Destroy(gameObject);
-            gm.GiveStar();
-            
+            gm.GiveStar();    
         }
     }
 
