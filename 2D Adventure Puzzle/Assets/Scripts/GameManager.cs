b64311ba68b6bcb1ad5a0597600171c001a46426
Robert <roobtran@gmail.com>
2019-03-07 03:04:52 -0500

    +Fixed all stages and more
    +Fixed all stages and more

--- 2D Adventure Puzzle/Assets/Scripts/GameManager.cs
+++ 2D Adventure Puzzle/Assets/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
     public AudioSource deathSound;
     public static bool GameIsPaused = true;
     public GameObject loadingPanel;
-    [SerializeField] int starCount = 0;
+    public int starCount = 0;
     public GameObject menuMain;
     public GameObject menuMain2;
     public GameObject menuTitle;
