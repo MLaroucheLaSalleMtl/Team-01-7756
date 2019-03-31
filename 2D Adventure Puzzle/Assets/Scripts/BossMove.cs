2b15c947d95d5b5e324966164e77ad577d67f799
Robert <roobtran@gmail.com>
2019-03-31 00:07:36 -0400

    +Added dialogue and event on cut scene
    +Added dialogue and event on cut scene

---
+++ 2D Adventure Puzzle/Assets/Scripts/BossMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossMove : MonoBehaviour
+{
+
+    public Animator bossAnime;
+    [SerializeField] Transform zPoint;
+    [SerializeField] CutSceneOne player;
+    [SerializeField] GameObject space;
+    public Text message;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bossAnime = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.x == zPoint.position.x)
+        {
+
+            //player.anime.SetBool("Reach", true);
+            bossAnime.SetBool("Moving", true);
+            StartCoroutine("Vortex");
+            
+        }
+    }
+
+    IEnumerator Vortex()
+    {
+        yield return new WaitForSeconds(2f);
+        space.SetActive(true);
+       
+    }
+
+
+
+}
